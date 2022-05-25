7444dc8b08084c6e39d5383c296e788714826d0c
uw0606 <103592210+uw0606@users.noreply.github.com>
2022-05-25 20:58:46 +0900

    Add files via upload

---
+++ NOBUTO/ZONSonusCustom4/js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+// スライドショー
+{
+  const images = [
+    'img/a.jpg',
+    'img/b.jpg',
+    'img/c.jpg',
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('zonsonuscustom4');
+  mainImage.src = images[currentIndex];
+
+  images.forEach((image, index) => {
+    const img = document.createElement('img');
+    img.src = image;
+
+    const li = document.createElement('li');
+    if (index === currentIndex) {
+      li.classList.add('current');
+    }
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+      const thumbnails = document.querySelectorAll('.thumbnails > li');
+      thumbnails[currentIndex].classList.remove('current');
+      currentIndex = index;
+      thumbnails[currentIndex].classList.add('current');
+    });
+
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if (target === images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+
+  const prev = document.getElementById('prev');
+  prev.addEventListener('click', () => {
+    let target = currentIndex - 1;
+    if (target < 0) {
+      target = images.length - 1;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+}
